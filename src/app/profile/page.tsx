f556db6d72c84b00f02c237204f3f662b53dc43e
antasth <antasth@mail.ru>
2023-11-21 23:38:36 +0300

    refactor: change navigation from router.push to Link components

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import styles from './page.module.css'
 export default function Profile() {
@@ -10,9 +11,14 @@ export default function Profile() {
     <div>
       <h1>PROFILES LIST</h1>
       <ul className={styles.list}>
-        <li onClick={() => router.push('/profile/1')}>profile 1</li>
+        <Link href={'/profile/1'}>Profile 1</Link>
+        <Link href={'/profile/2'}>Profile 2</Link>
+        <Link href={'/profile/3'}>Profile 3</Link>
+
+        {/*navigation using router */}
+        {/* <li onClick={() => router.push('/profile/1')}>profile 1</li>
         <li onClick={() => router.push('/profile/2')}>profile 2</li>
-        <li onClick={() => router.push('/profile/3')}>profile 3</li>
+        <li onClick={() => router.push('/profile/3')}>profile 3</li> */}
       </ul>
     </div>
   )
