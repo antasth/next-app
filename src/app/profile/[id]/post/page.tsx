654a079229f18d9b4daff5072b278e25376d8c4d
antasth <antasth@mail.ru>
2023-11-23 22:46:00 +0300

    refactor: change project structure

--- src/app/profile/[id]/post/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ProfileIdPosts() {
-  return <div>ID PROFILE POSTS PAGE</div>
-}
--- src/components/Header/Menu.tsx
+++
@@ -1,8 +0,0 @@
-export default function Menu(props: any) {
-  return (
-    <div>
-      {props.name}
-      {props.age}
-    </div>
-  )
-}
